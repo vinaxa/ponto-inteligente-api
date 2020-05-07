5109d34ad101b9b241275ba4fbd2e410c5b8e85c
vinaxa <vinaxa@gmail.com>
2020-05-07 05:55:58 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/cirrus_farm_technology/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cirrus_farm_technology.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
