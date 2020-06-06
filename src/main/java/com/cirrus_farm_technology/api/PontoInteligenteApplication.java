ab6ab0b10609dfb36395a38f6bceb91c1a999698
vinaxa <vinaxa@gmail.com>
2020-06-05 21:28:12 -0300

    Adicionando ehcache

--- src/main/java/com/cirrus_farm_technology/api/PontoInteligenteApplication.java
+++ src/main/java/com/cirrus_farm_technology/api/PontoInteligenteApplication.java
@@ -2,9 +2,11 @@ package com.cirrus_farm_technology.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 
 @SpringBootApplication//(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
+@EnableCaching
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/db/migration/postgresql/V2__admin_padrao.sql
@@ -0,0 +1,5 @@
+INSERT INTO empresa (cnpj, data_atualizacao, data_criacao, razao_social) 
+VALUES ('82198127000121', CURRENT_DATE, CURRENT_DATE, 'Swagger IT');
+
+INSERT INTO funcionario (cpf, data_atualizacao, data_criacao, email, nome, perfil, qtd_horas_almoco, qtd_horas_trabalho_dia, senha, valor_hora, empresa_id) 
+VALUES ('16248890935', CURRENT_DATE, CURRENT_DATE, 'admin@swagger.com', 'ADMIN', 'ROLE_ADMIN', NULL, NULL, '$2a$06$xIvBeNRfS65L1N17I7JzgefzxEuLAL0Xk0wFAgIkoNqu9WD6rmp4m', NULL, (SELECT id FROM empresa WHERE cnpj = '82198127000121'));
